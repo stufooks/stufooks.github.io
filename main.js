c053f9d1c2442e6326582a66821638fd154f7681
Stuart Fooks <sb.fooks@gmail.com>
2019-02-19 19:41:56 -0500

    major site update

--- main.js
+++ main.js
@@ -1,31 +1,8 @@
-//transform hamburger menu and show nav links on click
-let bars = document.querySelectorAll('.hamburger-container div')
-let hamburgerContainer = document.querySelector('.hamburger-container')
-let linksContainer = document.querySelector('.links-container')
+let emailLogo = document.querySelector('.email-logo')
+let email = document.querySelector('.email')
 
-const navClickHandler = () => {
-    for (let i = 0; i < bars.length; i++) {
-        bars[i].classList.toggle(`change${i + 1}`)
-    }
-    linksContainer.classList.toggle('display-nav')
+const clickHandler = () => {
+  email.classList.toggle('email-open')
 }
 
-hamburgerContainer.addEventListener('click', navClickHandler)
-
-
-// //display social icons on click
-// let name = document.querySelector('.name')
-// let iconsContainer = document.querySelector('.icons-container')
-// let contactLink = document.querySelector('.contact-link')
-
-// const nameClickHandler = (evt) => {
-//     let target = evt.target.tagName
-//     if (target === 'H1') {
-//         iconsContainer.classList.toggle('display-icons')
-//     } else {
-//         iconsContainer.classList.add('display-icons')
-//     }
-// }
-
-// name.addEventListener('click', nameClickHandler)
-// contactLink.addEventListener('click', nameClickHandler)
+emailLogo.addEventListener('click', clickHandler)
